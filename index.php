cfa2bd1c28476946bb76d23516e0c6dcbea557ad
Mr. Meo <20571336+trananhmanh89@users.noreply.github.com>
2020-08-28 10:17:50 +0700

    require database login to use this file

--- index.php
+++ index.php
@@ -8,33 +8,65 @@ class App
     public $protected = array();
     public $config;
     public $isLocal = false;
+    public $connected = false;
+    public $connectError = false;
 
     public function __construct()
     {
+        if (!file_exists(__DIR__ . '/config.json')) {
+            $data = json_encode(array(
+                'protected' => array(),
+                'db' => array(
+                    'username' => 'root',
+                    'password' => '',
+                )
+            ));
+            file_put_contents(__DIR__ . '/config.json', $data);
+        }
+
+        $this->config = @json_decode(@file_get_contents(__DIR__ . '/config.json'));
+        
         $whitelist = array(
             '127.0.0.1',
             '::1'
         );
 
-        if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+        if (in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
             $this->isLocal = true;
         }
 
-        $this->db = new mysqli("localhost", "root", "root");
-        if ($this->db->connect_errno) {
-            echo "Failed to connect to MySQL: " . $this->db->connect_error;
-        }
+        $task = $this->getInput('task', 'post');
+        if ($task === 'connect') {
+            $username = $this->getInput('username', 'post');
+            $password = $this->getInput('password', 'post');
 
-        if (!file_exists(__DIR__ . '/config.json')) {
-            file_put_contents(__DIR__ . '/config.json', json_encode(array('protected' => array())));
-        }
+            $this->db = @mysqli_connect('localhost', $username, $password);
 
-        $this->config = @json_decode(@file_get_contents(__DIR__ . '/config.json'));
+            if ($this->db) {
+                $this->connected = true;
+                $db = array(
+                    'username' => $username,
+                    'password' => $password,
+                );
+
+                $this->config->db = $db;
+
+                file_put_contents(__DIR__ . '/config.json', json_encode($this->config));
+            } else {
+                $this->connectError = true;
+            }
+        } else {
+            $username = $this->config->db->username;
+            $password = $this->config->db->password;
+            $this->db = @mysqli_connect('localhost', $username, $password);
+            
+            $this->connected = !!$this->db;
+        }
     }
 
     public function run()
     {
-        if (!$this->isLocal) {
+        if (!$this->isLocal || !$this->connected) {
             return;
         }
 
@@ -61,10 +93,10 @@ class App
                 exit();
         }
 
-        $result = mysqli_query($this->db,"SHOW DATABASES");
+        $result = mysqli_query($this->db, "SHOW DATABASES");
         $core = array('information_schema', 'mysql', 'performance_schema', 'sys');
         $protected = array_merge($this->config->protected, $core);
-        
+
         while ($row = mysqli_fetch_array($result)) {
             if (!in_array($row[0], $protected)) {
                 $this->cleanable[] = $row[0];
@@ -85,7 +117,7 @@ class App
     protected function removeProtect($name)
     {
         if (in_array($name, $this->config->protected)) {
-            $protected = array_filter($this->config->protected, function($item)  use ($name) {
+            $protected = array_filter($this->config->protected, function ($item)  use ($name) {
                 return $item !== $name;
             });
 
@@ -118,31 +150,32 @@ class App
 $app = new App;
 $app->run();
 ?>
-    <!DOCTYPE html>
-    <html>
+<!DOCTYPE html>
+<html>
 
-    <head>
-        <title>localhost</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <style>
-            .loading {
-                position: absolute; 
-                top: -10px; 
-                bottom: -10px; 
-                left: -10px; 
-                right: -10px; 
-                display: none;
-                justify-content: center;
-                align-items: center;
-                background-color: rgba(221, 221, 221, 0.3686274509803922);
-                font-style: italic;
-            }
-        </style>
-    </head>
+<head>
+    <title>localhost</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <style>
+        .loading {
+            position: absolute;
+            top: -10px;
+            bottom: -10px;
+            left: -10px;
+            right: -10px;
+            display: none;
+            justify-content: center;
+            align-items: center;
+            background-color: rgba(221, 221, 221, 0.3686274509803922);
+            font-style: italic;
+        }
+    </style>
+</head>
 
-    <body>
+<body>
+    <?php if ($app->connected) : ?>
         <div class="container">
             <h3 class="text-center">(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ (◕‿◕✿)</h3>
             <pre class="text-center"><?php print($_SERVER['SERVER_SOFTWARE']); ?></pre>
@@ -164,13 +197,13 @@ $app->run();
                         <tbody>
                             <?php $dirs = glob('*', GLOB_ONLYDIR); ?>
                             <?php foreach ($dirs as $key => $dir) : ?>
-                            <tr>
-                                <th><?php echo $key + 1 ?></th>
-                                <td>
-                                    <a title="<?php echo $dir ?>" href="<?php echo '/' . $dir ?>"><?php echo $dir ?></a>
-                                </td>
-                                <!-- <td>delete</td> -->
-                            </tr>
+                                <tr>
+                                    <th><?php echo $key + 1 ?></th>
+                                    <td>
+                                        <a title="<?php echo $dir ?>" href="<?php echo '/' . $dir ?>"><?php echo $dir ?></a>
+                                    </td>
+                                    <!-- <td>delete</td> -->
+                                </tr>
                             <?php endforeach; ?>
                         </tbody>
                     </table>
@@ -190,19 +223,19 @@ $app->run();
                                 <th></th>
                             </thead>
                             <tbody class="cleanable-list">
-                                <?php foreach ($app->cleanable as $db): ?>
-                                <tr class="cleanable-item item-<?php echo $db ?>">
-                                    <td><input class="cleanable-checkbox" type="checkbox" value="<?php echo $db ?>"></td>
-                                    <td class="db-name"><?php echo $db ?></td>
-                                    <td>
-                                        <a class="do-protect" href="javascript:;">protect</a>
-                                    </td>
-                                </tr>
+                                <?php foreach ($app->cleanable as $db) : ?>
+                                    <tr class="cleanable-item item-<?php echo $db ?>">
+                                        <td><input class="cleanable-checkbox" type="checkbox" value="<?php echo $db ?>"></td>
+                                        <td class="db-name"><?php echo $db ?></td>
+                                        <td>
+                                            <a class="do-protect" href="javascript:;">protect</a>
+                                        </td>
+                                    </tr>
                                 <?php endforeach ?>
                             </tbody>
                         </table>
                         <button type="button" class="btn btn-danger delete-btn">Delete</button>
-                        <div class="loading">processing</div>
+                        <div class="loading"></div>
                     </div>
                     <br>
                     <br>
@@ -214,13 +247,17 @@ $app->run();
                             <th></th>
                         </thead>
                         <tbody class="protected-list">
-                            <?php foreach ($app->config->protected as $db): ?>
-                            <tr class="protected-item">
-                                <td class="db-name"><?php echo $db ?></td>
-                                <td>
-                                    <a class="do-remove-protect" href="javascript:;">remove</a>
-                                </td>
-                            </tr>
+                            <?php
+                            $list = $app->config->protected;
+                            sort($list);
+                            ?>
+                            <?php foreach ($list as $db) : ?>
+                                <tr class="protected-item">
+                                    <td class="db-name"><?php echo $db ?></td>
+                                    <td>
+                                        <a class="do-remove-protect" href="javascript:;">remove</a>
+                                    </td>
+                                </tr>
                             <?php endforeach ?>
                         </tbody>
                     </table>
@@ -249,8 +286,7 @@ $app->run();
                     $('.cleanable-checkbox').prop('checked', checked);
                 });
 
-                function onClickProtect() 
-                {
+                function onClickProtect() {
                     var $elm = $(this).parents('.cleanable-item');
 
                     if ($elm.data('processing')) {
@@ -261,36 +297,35 @@ $app->run();
                     var name = $elm.find('.db-name').text().trim();
 
                     $.ajax({
-                        url: '/',
-                        data: {
-                            q: 'doProtect',
-                            name: name,
-                        },
-                    })
-                    .done(function() {
-                        var html = [
-                            '<tr class="protected-item">',
+                            url: '/',
+                            data: {
+                                q: 'doProtect',
+                                name: name,
+                            },
+                        })
+                        .done(function() {
+                            var html = [
+                                '<tr class="protected-item">',
                                 '<td class="db-name">' + name + '</td>',
                                 '<td>',
-                                    '<a class="do-remove-protect" href="javascript:;">remove</a>',
+                                '<a class="do-remove-protect" href="javascript:;">remove</a>',
                                 '</td>',
-                            '</tr>',
-                        ].join('');
+                                '</tr>',
+                            ].join('');
 
-                        var $item = $(html);
+                            var $item = $(html);
 
-                        $item.find('.do-remove-protect').on('click', onClickRemoveProtect);
+                            $item.find('.do-remove-protect').on('click', onClickRemoveProtect);
 
-                        $protectedList.append($item);
+                            $protectedList.append($item);
 
-                        $elm.remove();
-                    });
+                            $elm.remove();
+                        });
                 }
 
-                function onClickRemoveProtect() 
-                {
+                function onClickRemoveProtect() {
                     var $elm = $(this).parents('.protected-item');
-                    
+
                     if ($elm.data('processing')) {
                         return;
                     }
@@ -300,32 +335,32 @@ $app->run();
                     var name = $elm.find('.db-name').text().trim();
 
                     $.ajax({
-                        url: '/',
-                        data: {
-                            q: 'removeProtect',
-                            name: name,
-                        },
-                    })
-                    .done(function() {
-                        var html = [
-                            '<tr class="cleanable-item item-' + name + '">',
+                            url: '/',
+                            data: {
+                                q: 'removeProtect',
+                                name: name,
+                            },
+                        })
+                        .done(function() {
+                            var html = [
+                                '<tr class="cleanable-item item-' + name + '">',
                                 '<td><input class="cleanable-checkbox" type="checkbox" value="' + name + '"></td>',
                                 '<td class="db-name">' + name + '</td>',
                                 '<td>',
-                                    '<a class="do-protect" href="javascript:;">protect</a>',
+                                '<a class="do-protect" href="javascript:;">protect</a>',
                                 '</td>',
-                            '</tr>',
-                        ].join('');
+                                '</tr>',
+                            ].join('');
 
-                        var $item = $(html);
+                            var $item = $(html);
 
-                        $item.find('.do-protect').on('click', onClickProtect);
-                        $item.find('.cleanable-checkbox').on('click', onChangeCleanableCheckbox);
+                            $item.find('.do-protect').on('click', onClickProtect);
+                            $item.find('.cleanable-checkbox').on('click', onChangeCleanableCheckbox);
 
-                        $cleanableList.append($item);
-                        
-                        $elm.remove();
-                    });
+                            $cleanableList.append($item);
+
+                            $elm.remove();
+                        });
                 }
 
                 $doProtect.on('click', onClickProtect);
@@ -363,26 +398,70 @@ $app->run();
                     var $elm = $('.item-' + name);
 
                     $.ajax({
-                        url: '/',
-                        data: {
-                            q: 'delete',
-                            name: name,
-                        },
-                        dataType: 'json',
-                    })
-                    .done(function(res) {
-                        if (res.error) {
-                            alert('Failed to delete database "'+name+'"! \nERROR: ' + res.error);
-                            $loading.hide();
-                            $deleteBtn.text('Delete');
-                        } else {
-                            $elm.remove();
-                            runQueue(list);
-                        }
-                    });
+                            url: '/',
+                            data: {
+                                q: 'delete',
+                                name: name,
+                            },
+                            dataType: 'json',
+                        })
+                        .done(function(res) {
+                            if (res.error) {
+                                alert('Failed to delete database "' + name + '"! \nERROR: ' + res.error);
+                                $loading.hide();
+                                $deleteBtn.text('Delete');
+                            } else {
+                                $elm.remove();
+                                runQueue(list);
+                            }
+                        });
                 }
             });
         </script>
-    </body>
+    <?php else : ?>
+        <div class="container">
+            <br>
+            <h4>(っ＾▿＾)۶🍸🌟🍺٩(˘◡˘ ) </h4>
+            <br>
+            <h4>Database Connection</h4>
+            <br>
+            <?php if ($app->connectError): ?>
+                <div class="alert alert-danger" role="alert">
+                    Connect error. Please try again!
+                </div>
+            <?php else: ?>
+                <div class="alert alert-warning" role="alert">
+                    Connect database to use all features.
+                </div>
+            <?php endif ?>
+            <form action="index.php" method="post">
+                <div class="form-group row">
+                    <label for="host" class="col-sm-2 col-form-label">Host</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" autocomplete="off" name="host" value="localhost" disabled required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="username" class="col-sm-2 col-form-label">Username</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" autocomplete="off" name="username" required>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="password" class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" autocomplete="off" name="password">
+                    </div>
+                </div>
+                <input type="hidden" name="task" value="connect">
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <button type="submit" class="btn btn-primary">Connect</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    <?php endif ?>
+</body>
 
-    </html>
+</html>
