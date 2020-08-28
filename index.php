ac3ea6c0d185e0d63140cd61f86907497b6c1f12
Mr. Meo <20571336+trananhmanh89@users.noreply.github.com>
2020-08-28 10:47:17 +0700

    add favicon

--- index.php
+++ index.php
@@ -155,6 +155,7 @@ $app->run();
 
 <head>
     <title>localhost</title>
+    <link rel="icon" type="image/x-icon" href="https://cdnjs.cloudflare.com/ajax/libs/twemoji/12.0.4/16x16/26bd.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
